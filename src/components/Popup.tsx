df86a99702e53f22150f6285bfb2de644197f895
David Liu <msglight4874@gmail.com>
2020-03-22 19:09:32 +0800

    Add `Popup` component

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import {
+  List, ListItem, ListItemText, ListItemAvatar, Avatar, Button, Box, Divider, Typography,
+} from '@material-ui/core';
 
 import style from '@src/stylesheets/popup.scss';
 
-import Box from '@material-ui/core/Box';
+import StorageHandler, { StorageType } from '@src/utils/storageHandler';
+import Book from '@src/types/book';
 
-// import StorageHandler, { StorageType } from '@src/utils/storageHandler';
+const storageHandler = new StorageHandler();
 
-// const storageHandler = new StorageHandler();
-// const updateBooks = (books: unknown) => {
-// };
-// storageHandler.get(StorageType.Books).then(updateBooks);
-// storageHandler.subscribe(StorageType.Books, (_, newBooks) => { updateBooks(newBooks); });
+const Popup = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+  useEffect(() => {
+    storageHandler.get(StorageType.Books).then(setBooks);
 
-const Popup = () => (
-  <Box className={style.popup} color="white" bgcolor="palevioletred">
-    Hello, world!
-  </Box>
-);
+    storageHandler.subscribe(StorageType.Books, (_, newBooks) => { setBooks(newBooks as Book[]); });
+  }, []);
+  const noBooks = books.length === 0;
+
+  if (noBooks) {
+    return (
+      <Typography variant="h6" noWrap>
+        <Box textAlign="center" mx={2} m={1}>
+          沒有可供下載書籍
+        </Box>
+      </Typography>
+    );
+  }
+
+  return (
+    <Box className={style.popup}>
+      <List>
+        { books.map(book => (
+          <Fragment key={book.id}>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar variant="square" alt={book.title} src={book.coverImageUrl} />
+              </ListItemAvatar>
+              <ListItemText primary={book.title} />
+              <Button variant="contained" color="primary">
+                下載
+              </Button>
+            </ListItem>
+            <Divider variant="inset" component="li" />
+          </Fragment>
+        )) }
+      </List>
+    </Box>
+  );
+};
 export default Popup;
