cbfabc103d423305db815304f82f0eec4631c697
David Liu <msglight4874@gmail.com>
2020-03-23 10:25:54 +0800

    Remove `MutationObserver` to detect books

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,5 +1,3 @@
-import debounce from 'lodash/debounce';
-
 import Detectors from '@src/detectors';
 import StorageHandler, { StorageType } from './utils/storageHandler';
 import EventHandler, { EventType } from './utils/eventHandler';
@@ -8,24 +6,13 @@ import EventHandler, { EventType } from './utils/eventHandler';
   const storageHandler = new StorageHandler();
   const eventHandler = new EventHandler();
   const detectors = Detectors.map(Detector => new Detector(window));
-  const detectBooks = debounce(() => {
+  const detectBooks = () => {
     const exportableDetector = detectors.find(detector => detector.isExportable());
     const books = exportableDetector ? exportableDetector.extractBooks() : [];
 
     storageHandler.set(StorageType.Books, books);
-  }, 300);
+  };
 
-  // By manuall event
+  // By manual event
   eventHandler.subscribe(EventType.DetectBooks, detectBooks);
-
-  // By DOM change
-  const mutationObserver = new MutationObserver(detectBooks);
-  mutationObserver.observe(document, {
-    childList: true,
-    attributes: true,
-    characterData: true,
-    subtree: true,
-    attributeOldValue: true,
-    characterDataOldValue: true,
-  });
 })();
--- test/contentScript.test.ts
+++ test/contentScript.test.ts
@@ -7,9 +7,6 @@ const isExportable = jest.fn();
 const extractBooks = jest.fn();
 const set = jest.spyOn(StorageHandler.prototype, 'set').mockImplementation();
 
-// for test debounce function
-jest.mock('lodash/debounce', () => jest.fn(fn => fn));
-
 // mock @src/detectors
 class MockDetector {
   public isExportable = isExportable;
@@ -17,13 +14,6 @@ class MockDetector {
   public extractBooks = extractBooks;
 }
 jest.setMock('@src/detectors', [MockDetector]);
-
-// mock MutationObserver
-type MutationObserver = typeof window.MutationObserver;
-// eslint-disable-next-line func-names, prefer-arrow-callback
-const MockMutationObserver = jest.fn(function (callback: Function) { callback(); });
-MockMutationObserver.prototype = { observe: jest.fn() };
-window.MutationObserver = MockMutationObserver as unknown as MutationObserver;
 
 const loadContentScript = () => jest.isolateModules(() => {
   // eslint-disable-next-line global-require
@@ -33,12 +23,15 @@ const loadContentScript = () => jest.isolateModules(() => {
 describe('contentScript', () => {
   beforeEach(() => { window.chrome = mockChrome(); });
   afterEach(() => { delete window.chrome; });
+
   it('send Books to extension when any exportable detector found', () => {
+    const eventHandler = new EventHandler(window.chrome);
     const books = [{ id: 'book double' }];
     isExportable.mockImplementationOnce(() => true);
     extractBooks.mockImplementationOnce(() => books);
 
     loadContentScript();
+    eventHandler.sendToActiveTab(EventType.DetectBooks);
 
     expect(isExportable).toHaveBeenCalled();
     expect(extractBooks).toHaveBeenCalled();
@@ -46,32 +39,16 @@ describe('contentScript', () => {
   });
 
   it('send empty array to extension when no exportable detector found', () => {
+    const eventHandler = new EventHandler(window.chrome);
     const books: Book[] = [];
     isExportable.mockImplementationOnce(() => false);
     extractBooks.mockImplementationOnce(() => books);
 
     loadContentScript();
+    eventHandler.sendToActiveTab(EventType.DetectBooks);
 
     expect(isExportable).toHaveBeenCalled();
     expect(extractBooks).not.toHaveBeenCalled();
-    expect(set).toBeCalledWith(StorageType.Books, books);
-  });
-
-  it('trigger by DetectBook event', () => {
-    const books = [{ id: 'book double' }];
-    const eventHandler = new EventHandler(window.chrome);
-    isExportable.mockImplementation(() => true);
-    extractBooks.mockImplementation(() => books);
-
-    // first Detect
-    loadContentScript();
-
-    // second Detect
-    eventHandler.sendToActiveTab(EventType.DetectBooks);
-
-    expect(isExportable).toHaveBeenCalledTimes(2);
-    expect(extractBooks).toHaveBeenCalledTimes(2);
-    expect(set).toBeCalledTimes(2);
     expect(set).toBeCalledWith(StorageType.Books, books);
   });
 });
