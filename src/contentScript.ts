872b6e6209ad9ba5317b84b950dfefc11c75b541
David Liu <msglight4874@gmail.com>
2020-03-24 21:15:30 +0800

    Remove unnecessary `IIFE`

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -2,17 +2,15 @@ import Detectors from '@src/detectors';
 import StorageHandler, { StorageType } from './utils/storageHandler';
 import EventHandler, { EventType } from './utils/eventHandler';
 
-(() => {
-  const storageHandler = new StorageHandler();
-  const eventHandler = new EventHandler();
-  const detectors = Detectors.map(Detector => new Detector(window));
-  const detectBooks = () => {
-    const exportableDetector = detectors.find(detector => detector.isExportable());
-    const books = exportableDetector ? exportableDetector.extractBooks() : [];
+const storageHandler = new StorageHandler();
+const eventHandler = new EventHandler();
+const detectors = Detectors.map(Detector => new Detector(window));
+const detectBooks = () => {
+  const exportableDetector = detectors.find(detector => detector.isExportable());
+  const books = exportableDetector ? exportableDetector.extractBooks() : [];
 
-    storageHandler.set(StorageType.Books, books);
-  };
+  storageHandler.set(StorageType.Books, books);
+};
 
-  // By manual event
-  eventHandler.subscribe(EventType.DetectBooks, detectBooks);
-})();
+// By manual event
+eventHandler.subscribe(EventType.DetectBooks, detectBooks);
