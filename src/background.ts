e89c4bb9fd9b59e96b2e2e199975a7ce52e22191
David Liu <msglight4874@gmail.com>
2020-04-02 13:09:56 +0800

    Change `blobToDataUrl` to `URL.createObjectURL` for better performance

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,5 @@
 import EventHandler, { EventType } from '@src/utils/eventHandler';
 import Book from '@src/types/book';
-import blobToDataUrl from '@src/utils/blobToDataUrl';
 import { exporterSelector } from '@src/exporters';
 import StorageHandler, { StorageType } from '@src/utils/storageHandler';
 
@@ -11,8 +10,8 @@ eventHandler.subscribe(EventType.DownloadBook, async (book: Book) => {
   try {
     const exporter = exporterSelector(book);
     const blob = await exporter.export();
-    const dataUrl = await blobToDataUrl(blob);
-    chrome.downloads.download({ url: dataUrl, filename: `${book.title}.epub` });
+    const objectUrl = URL.createObjectURL(blob);
+    chrome.downloads.download({ url: objectUrl, filename: `${book.title}.epub` });
   } catch (e) {
     // @TODO: Error handler
     // eslint-disable-next-line no-console
